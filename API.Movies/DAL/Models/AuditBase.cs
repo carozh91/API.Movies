5616dd849b5fac76166d7208dadfc7a2042bdd47
carozh91 <165485810+carozh91@users.noreply.github.com>
2025-11-08 11:21:29 -0500

    Agregar archivos de proyecto.

---
+++ API.Movies/DAL/Models/AuditBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Movies.DAL.Models
+{
+    public class AuditBase
+    {
+
+        [Key]
+        public virtual int Id { get; set; }
+
+        public virtual DateTime CreatedDate { get; set; }
+
+        public virtual DateTime ModifiedDate { get; set; }
+
+    }
+}
---
+++ API.Movies/DAL/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Movies.DAL.Models
+{
+    public class Category : AuditBase
+    {
+
+       
+        [Required]
+        public string Name { get; set; }
+    }
+}
