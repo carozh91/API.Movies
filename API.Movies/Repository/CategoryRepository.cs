f474254cf73584f4698558cab3846b2db92e8686
carozh91 <165485810+carozh91@users.noreply.github.com>
2025-11-08 15:38:25 -0500

    Primer guardado de API

---
+++ API.Movies/Repository/CategoryRepository.cs
@@ -0,0 +1,56 @@
+﻿using API.Movies.DAL;
+using API.Movies.DAL.Models;
+using API.Movies.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Movies.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        //patron creacion de dependencias:
+
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> CategoryExistsByIdAsync(int id)
+        
+        {
+            return await _context.Categories.AsNoTracking().AnyAsync(c => c.Id == id);
+        }
+
+        public async Task<bool> CategoryExistsByNameAsync(string name)
+        {
+            return await _context.Categories.AsNoTracking().AnyAsync(c => c.Name == name);
+        }
+
+        public Task<bool> CreateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ICollection<Category>> GetCategoriesAsync()
+        {
+            return await _context.Categories.AsNoTracking().OrderBy(c => c.Name).ToListAsync();
+
+        }
+
+        public async Task<Category> GetCategoryAsync(int id)//async y await para operaciones asincronas
+        {
+            return await _context.Categories.AsNoTracking().FirstOrDefaultAsync(c => c.Id == id);
+
+            
+        }
+
+        public Task<bool> UpdateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API.Movies/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using API.Movies.DAL.Models;
+
+namespace API.Movies.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<ICollection<Category>> GetCategoriesAsync(); //Me retorna lista de categorias
+        Task<Category>GetCategoryAsync(int id);//Me retorna una categoría por ID
+        Task<bool> CategoryExistsByIdAsync(int id);//Me retorna si existe una cat pot ID
+        Task<bool> CategoryExistsByNameAsync(string name);//Existe una cat por Name
+        Task<bool> CreateCategoryAsync(Category category);//Crea una categoría
+
+        Task<bool> DeleteCategoryAsync(int id);
+
+        Task<bool> UpdateCategoryAsync(Category category);
+        
+
+
+
+    }
+}
