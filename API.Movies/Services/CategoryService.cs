2419741500159fab0f542b91030d0bb3c7995b00
carozh91 <165485810+carozh91@users.noreply.github.com>
2025-11-09 15:09:55 -0500

    New changes on the API.

---
+++ API.Movies/Services/CategoryService.cs
@@ -0,0 +1,49 @@
+﻿using API.Movies.DAL.Models;
+using API.Movies.Services.IServices;
+
+namespace API.Movies.Services
+{
+    public class CategoryService: ICategoryService
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryService()
+        {
+            
+        }
+
+        Task<bool> ICategoryService.CategoryExistsByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> ICategoryService.CategoryExistsByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> ICategoryService.CreateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> ICategoryService.DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ICollection<Category>> ICategoryService.GetCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Category> ICategoryService.GetCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> ICategoryService.UpdateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
