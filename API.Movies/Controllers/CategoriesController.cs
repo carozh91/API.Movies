8d43b73824de4d00c91d4dda3a1493788d6b5ae9
carozh91 <165485810+carozh91@users.noreply.github.com>
2025-11-14 11:31:03 -0500

    update controllers

--- API.Movies/Controllers/CategoriesController.cs
+++ API.Movies/Controllers/CategoriesController.cs
@@ -20,11 +20,21 @@ namespace API.Movies.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ICollection<CategoryDto>>> GetCategoriesAsync()
         {
             var categories = await _categoryService.GetCategoriesAsync();
             return Ok(categories);
-        }   
-    }
+        }
 
+        [HttpGet("{id:int}", Name = "GetCategoryAsync")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<CategoryDto>> GetCategoryAsync(int id)
+        {
+            var categoryDto = await _categoryService.GetCategoryAsync(id);
+            return Ok(categoryDto);
+        }
+    }
 }
